b10f087c07f42f8399d4ff0e46705f2bf20d95a0
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-03-06 08:25:54 +0000

    wip
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

---
+++ .github/renovate-config.js
@@ -0,0 +1,65 @@
+"use strict";
+// https://github.com/renovatebot/github-action/blob/main/.github/renovate.json
+// https://docs.renovatebot.com/configuration-options/
+
+const fs = require('fs');
+const dry_run = process.env.RENOVATE_DRY_RUN
+console.log(`DRY_RUN mode: ${dry_run}`);
+
+// console.log(process.env)
+
+module.exports = {
+	"extends": [":disableRateLimiting", ":semanticCommits"],
+	"assigneesFromCodeOwners": true,
+	"gitAuthor": "Renovate Bot <bot@external-dns.com>",
+	"onboarding": false,
+	"platform": "github",
+	"repositories": [
+		// "kubernetes-sigs/external-dns",
+		"ik-workshop/external-dns-fork-renovate-bootstrap"
+	],
+	"printConfig": true,
+	"prConcurrentLimit": 0,
+	"prHourlyLimit": 0,
+	"stabilityDays": 3,
+	"pruneStaleBranches": true,
+	"recreateClosed": true,
+	"dependencyDashboard": false,
+	"requireConfig": false,
+	"rebaseWhen": "behind-base-branch",
+	"baseBranches": ["master", "main"],
+	"recreateWhen": "always",
+	"semanticCommits": "enabled",
+	"pre-commit": {
+		"enabled": true
+	},
+	"additionalBranchPrefix": "{{packageFileDir}}-",
+	"labels": ["{{depType}}", "datasource::{{datasource}}", "type::{{updateType}}", "manager::{{manager}}"], // can be overridden per packageRule
+	"addLabels": ["renovate-bot"], // cannot be overridden, any packageRule config extends this
+	"packageRules": [
+		{
+			"groupName": "pre-commit",
+			"matchManagers": ["pre-commit"],
+			"addLabels": ["pre-commit", "skip-release"]
+		},
+	],
+	"enabledManagers": [ // supported managers https://docs.renovatebot.com/modules/manager/
+		"regex",
+		"pre-commit"
+	],
+	"customManagers": [ // https://docs.renovatebot.com/modules/manager/regex/
+		{
+			// to capture registry.k8s.io/external-dns/external-dns:<version> in *.md files
+			"customType": "regex",
+			"fileMatch": [
+				".*\\.md$"
+			],
+			"matchStrings": [
+				"(?<depName>registry.k8s.io\/external-dns\/external-dns):(?<currentValue>.*)"
+			],
+			"depNameTemplate": "kubernetes-sigs/external-dns",
+			"datasourceTemplate": "github-releases",
+			"versioningTemplate": "semver"
+		}
+	]
+};
